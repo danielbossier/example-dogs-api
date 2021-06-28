6cb3323705cf8ef7acb8a97c35dc36cfdf9ff323
danielbossier <dbossier21@gmail.com>
2021-06-28 13:00:33 -0700

    started create method

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+  def create
+    if current_user
+      dog = Dog.new(
+        name: params[:name],
+        age: params[:age],
+        breed: params[:breed],
+        user_id: current_user.id,
+      )
+      dog.save
+      render json: dog.as_json
+    end
+  end
 end
